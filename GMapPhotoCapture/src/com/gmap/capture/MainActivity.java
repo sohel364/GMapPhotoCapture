8fe944f26d9ba52b8309ef977ac1d03c0d90db9d
sohel364 <sohel.official@gmail.com>
2015-05-09 17:50:55 +0600

    adding google-play-services.jar

--- GMapPhotoCapture/src/com/gmap/capture/MainActivity.java
+++ GMapPhotoCapture/src/com/gmap/capture/MainActivity.java
@@ -3,9 +3,16 @@ package com.gmap.capture;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+
 
 public class MainActivity extends Activity {
-
+	  static final LatLng HAMBURG = new LatLng(53.558, 9.927);
+	  static final LatLng KIEL = new LatLng(53.551, 9.993);
+	  private GoogleMap map;
+	  
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
