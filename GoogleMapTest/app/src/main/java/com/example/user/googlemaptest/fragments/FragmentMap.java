36bda15575573391f4dbccc2a7b294a33840d3da
Tamal Tahsin <t.tahasin@gmail.com>
2015-05-21 22:52:18 +0600

    Added map fragment.

--- GoogleMapTest/app/src/main/java/com/example/user/googlemaptest/fragments/FragmentMap.java
+++ GoogleMapTest/app/src/main/java/com/example/user/googlemaptest/fragments/FragmentMap.java
@@ -1,22 +1,122 @@
 package com.example.user.googlemaptest.fragments;
 
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.user.googlemaptest.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * Created by user on 5/20/2015.
  */
 public class FragmentMap extends Fragment{
 
+    private GoogleMap mGoogleMap;
+    private SupportMapFragment mFragment;
+    private LocationManager mLocationManager;
+    private Marker mMarker;
+
+
+    private GoogleMap.OnMyLocationChangeListener myLocationChangeListener = new GoogleMap.OnMyLocationChangeListener() {
+        @Override
+        public void onMyLocationChange(Location location) {
+            LatLng loc = new LatLng(location.getLatitude(), location.getLongitude());
+            mMarker = mGoogleMap.addMarker(new MarkerOptions().position(loc));
+            if(mGoogleMap != null){
+                mGoogleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(loc, 16.0f));
+            }
+        }
+    };
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return  inflater.inflate(R.layout.fragment_map, container, false);
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+        loadCurrentLocationMap();
+        return  view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        FragmentManager fm = getChildFragmentManager();
+        mFragment = (SupportMapFragment) fm.findFragmentById(R.id.map_container);
+        if (mFragment == null) {
+            mFragment = SupportMapFragment.newInstance();
+            fm.beginTransaction().replace(R.id.map_container, mFragment).commit();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mGoogleMap == null) {
+            mGoogleMap = mFragment.getMap();
+            mGoogleMap.setMyLocationEnabled(true);
+            //mGoogleMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)));
+            //loadCurrentLocationMap();
+            mGoogleMap.setOnMyLocationChangeListener(myLocationChangeListener);
+        }
+    }
+
+    private void loadCurrentLocationMap() {
+        LocationManager service = (LocationManager) getActivity().getSystemService(Activity.LOCATION_SERVICE);
+        boolean enabledGPS = service
+                .isProviderEnabled(LocationManager.GPS_PROVIDER);
+        boolean enabledWiFi = service
+                .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+
+        // Check if enabled and if not send user to the GSP settings
+        // Better solution would be to display a dialog and suggesting to
+        // go to the settings
+        if (!enabledGPS) {
+            Toast.makeText(getActivity().getApplicationContext(), "GPS signal not found", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(intent);
+        }
+
+        mLocationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+
+        // Define the criteria how to select the locatioin provider -> use
+        // default
+        Criteria criteria = new Criteria();
+        String provider = mLocationManager.getBestProvider(criteria, false);
+        Location location = mLocationManager.getLastKnownLocation(provider);
+
+        LatLng currentLatlong = new LatLng(location.getLatitude(), location.getLongitude());
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(currentLatlong);
+        markerOptions.snippet("Current Locations");
+        markerOptions.title("Your Location");
+        if(mGoogleMap != null) {
+            mMarker = mGoogleMap.addMarker(markerOptions);
+            mGoogleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatlong, 15.0F));
+        }
+
+
+
     }
 }
