5f80020f09851155e493e50fc1dd680196142d2d
sohel364 <sohel.official@gmail.com>
2015-05-24 19:57:41 +0600

    Utils for HttpResponseData Call of Utils in FragmentGraph.java

--- GoogleMapTest/app/src/main/java/com/example/user/googlemaptest/Utilities/Utils.java
+++ GoogleMapTest/app/src/main/java/com/example/user/googlemaptest/Utilities/Utils.java
@@ -6,8 +6,31 @@ import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
+import android.os.Message;
 import android.provider.Settings;
 import android.support.v7.app.AlertDialog;
+import android.util.JsonReader;
+import android.util.JsonToken;
+import android.util.Log;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by user on 5/23/2015.
@@ -70,4 +93,50 @@ public class Utils {
         });
         alert.show();
     }
+
+
+
+    public static void HttpResponseData() throws IOException {
+
+        try {
+            /*
+                @Tamal We have to pass the params in the url it's static right now.
+             */
+            URL url = new URL("http://inzaana.com/WebBuilder/lon_lat_service.php?long_min=28.40&long_max=28.60&lat_min=77.10&lat_max=77.30");
+            HttpURLConnection con = (HttpURLConnection) url
+                    .openConnection();
+            readStream(con.getInputStream());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+      //  return lstString;
+    }
+
+    private  static List<String> readStream(InputStream in) {
+        BufferedReader reader = null;
+        List<String> lstString = new ArrayList<String>();
+        try {
+            reader = new BufferedReader(new InputStreamReader(in));
+            String line = "";
+
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+                lstString.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return lstString;
+    }
+
 }
--- GoogleMapTest/app/src/main/java/com/example/user/googlemaptest/fragments/FragmentGraph.java
+++ GoogleMapTest/app/src/main/java/com/example/user/googlemaptest/fragments/FragmentGraph.java
@@ -18,7 +18,9 @@ import com.androidplot.xy.SimpleXYSeries;
 import com.androidplot.xy.XYPlot;
 import com.androidplot.xy.XYSeries;
 import com.example.user.googlemaptest.R;
+import com.example.user.googlemaptest.Utilities.Utils;
 
+import java.io.IOException;
 import java.util.Arrays;
 
 /**
@@ -27,17 +29,28 @@ import java.util.Arrays;
 public class FragmentGraph extends Fragment{
 
     private XYPlot mySimpleXYPlot;
+    private View view;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view =  inflater.inflate(R.layout.fragment_graph, container, false);
+        view =  inflater.inflate(R.layout.fragment_graph, container, false);
+        LoadGraph();
 
+        try {
+            Utils.HttpResponseData();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+        return view;
+    }
+
+    public void LoadGraph(){
         PieChart graficoPartidos = (PieChart) view.findViewById(R.id.mySimplePieChart);
         graficoPartidos.getBackgroundPaint().setColor(Color.WHITE);
 
-        Segment seg0 = new Segment(" ", 0.0);
-        Segment seg1 = new Segment(" ", 10);
+        Segment seg0 = new Segment(" ", 2.0);
+        Segment seg1 = new Segment(" ", 5);
         Segment seg2 = new Segment(" ", 10);
 
         graficoPartidos.addSeries(seg0, new SegmentFormatter(Color.rgb(106, 168, 79), Color.BLACK,Color.BLACK, Color.BLACK));
@@ -45,7 +58,7 @@ public class FragmentGraph extends Fragment{
         graficoPartidos.addSeries(seg2, new SegmentFormatter(Color.rgb(255, 153, 0), Color.BLACK,Color.BLACK, Color.BLACK));
         PieRenderer pieRenderer = graficoPartidos.getRenderer(PieRenderer.class);
         pieRenderer.setDonutSize((float) 0 / 100, PieRenderer.DonutMode.PERCENT);
-
-        return view;
     }
+
+
 }
